2eb52165554873b410f3749e3b55459546e43c35
PascualVera <pascualveradev@gmail.com>
2022-11-01 20:11:25 +0100

    endpoints basic functioning made

--- PriceRequest/src/main/java/com/MicroService/PriceRequest/Controller/PriceRequestController.java
+++ PriceRequest/src/main/java/com/MicroService/PriceRequest/Controller/PriceRequestController.java
@@ -3,13 +3,13 @@ package com.MicroService.PriceRequest.Controller;
 
 import com.MicroService.PriceRequest.Repositories.PriceRequestRepository;
 import com.MicroService.PriceRequest.models.Trip;
-import org.springframework.data.jpa.repository.Query;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/trips")
@@ -30,8 +30,29 @@ public class PriceRequestController {
         return new ResponseEntity<>(trips, HttpStatus.ACCEPTED);
 
     }
-    @GetMapping(path = "/price")
 
-    public void getPrice() {
+    @GetMapping(path = "/origin={name}")
+    public ResponseEntity<List<Trip>> getByOrigin(@PathVariable String name) {
+
+        List<Trip> res = repository.findByOrigin_name(name);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+    }
+    @GetMapping(path = "/company={name}")
+    public ResponseEntity<List<Trip>> getByAirlines(@PathVariable String name) {
+
+        List<Trip> res = repository.findByCompany(name);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+
+    }
+    @GetMapping(path = "/id={id}")
+    public ResponseEntity<?> getById(@PathVariable int id) {
+        Optional<Trip> res = repository.findById(id);
+    if(res.isEmpty()){
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    } else {
+       return  new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+     }
     }
 }
--- PriceRequest/src/main/java/com/MicroService/PriceRequest/Repositories/PriceRequestRepository.java
+++ PriceRequest/src/main/java/com/MicroService/PriceRequest/Repositories/PriceRequestRepository.java
@@ -3,7 +3,10 @@ package com.MicroService.PriceRequest.Repositories;
 import com.MicroService.PriceRequest.models.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 
 public interface PriceRequestRepository extends JpaRepository<Trip, Integer> {
+    List<Trip> findByOrigin_name(String name);
+    List<Trip> findByCompany(String name);
 }
