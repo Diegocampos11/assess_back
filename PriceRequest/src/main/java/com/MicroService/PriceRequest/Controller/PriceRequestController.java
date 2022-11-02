2855645bd5443907060d389076d249bb43a0cb15
PascualVera <pascualveradev@gmail.com>
2022-11-02 10:22:15 +0100

    service/controller split made

--- PriceRequest/src/main/java/com/MicroService/PriceRequest/Controller/PriceRequestController.java
+++ PriceRequest/src/main/java/com/MicroService/PriceRequest/Controller/PriceRequestController.java
@@ -2,8 +2,10 @@ package com.MicroService.PriceRequest.Controller;
 
 
 import com.MicroService.PriceRequest.Repositories.PriceRequestRepository;
+import com.MicroService.PriceRequest.Service.PriceRequestService;
 import com.MicroService.PriceRequest.models.Trip;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -16,65 +18,58 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/trips")
-@Component
 public class PriceRequestController {
-    public PriceRequestRepository repository;
 
-    public PriceRequestController(PriceRequestRepository repository) {
+    @Autowired
+    private PriceRequestService service;
+
+    public PriceRequestController(PriceRequestService service) {
         super();
-        this.repository = repository;
+        this.service = service;
     }
 
     @CrossOrigin
-    @GetMapping (path = "/all")
-    public ResponseEntity<List<Trip>> test() {
-        List<Trip> trips = repository.findAll();
-
-        return new ResponseEntity<>(trips, HttpStatus.ACCEPTED);
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Trip>> getAll() {
+        return service.getAll();
 
     }
+
     @CrossOrigin
     @GetMapping(path = "/origin/{name}")
     public ResponseEntity<List<Trip>> getByOrigin(@PathVariable String name) {
-
-        List<Trip> res = repository.findByOrigin_name(name);
-        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
-
+        return service.getByOrigin(name);
     }
+
     @CrossOrigin
     @GetMapping(path = "/destination/{name}")
     public ResponseEntity<List<Trip>> getByDestiny(@PathVariable String name) {
 
-        List<Trip> res = repository.findByDestination_name(name);
-        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+        return service.getByDestiny(name);
 
     }
+
     @CrossOrigin
     @GetMapping(path = "/company/{name}")
     public ResponseEntity<List<Trip>> getByAirlines(@PathVariable String name) {
 
-        List<Trip> res = repository.findByCompany(name);
-        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+        return service.getByAirlines(name);
 
 
     }
+
     @CrossOrigin
     @GetMapping(path = "/date/{date}")
-    public ResponseEntity<List<Trip>> getByAirlines(@PathVariable LocalDate date) {
+    public ResponseEntity<List<Trip>> getByDate(@PathVariable LocalDate date) {
 
-        List<Trip> res = repository.findByDate(date);
-        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+        return service.getByDate(date);
 
 
     }
+
     @CrossOrigin
     @GetMapping(path = "/id/{id}")
     public ResponseEntity<?> getById(@PathVariable int id) {
-        Optional<Trip> res = repository.findById(id);
-    if(res.isEmpty()){
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    } else {
-       return  new ResponseEntity<>(res, HttpStatus.ACCEPTED);
-     }
+        return service.getById(id);
     }
 }
---
+++ PriceRequest/src/main/java/com/MicroService/PriceRequest/Service/PriceRequestService.java
@@ -0,0 +1,67 @@
+package com.MicroService.PriceRequest.Service;
+
+import com.MicroService.PriceRequest.Repositories.PriceRequestRepository;
+import com.MicroService.PriceRequest.models.Trip;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PriceRequestService {
+    public PriceRequestRepository repository;
+
+    public PriceRequestService(PriceRequestRepository repository) {
+        super();
+        this.repository = repository;
+    }
+
+    public ResponseEntity<List<Trip>> getAll() {
+        List<Trip> trips = repository.findAll();
+
+        return new ResponseEntity<>(trips, HttpStatus.ACCEPTED);
+
+    }
+
+    public ResponseEntity<List<Trip>> getByOrigin(String name) {
+
+        List<Trip> res = repository.findByOrigin_name(name);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+    }
+
+    public ResponseEntity<List<Trip>> getByDestiny(String name) {
+
+        List<Trip> res = repository.findByDestination_name(name);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+    }
+
+    public ResponseEntity<List<Trip>> getByAirlines(String name) {
+
+        List<Trip> res = repository.findByCompany(name);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+
+    }
+
+    public ResponseEntity<List<Trip>> getByDate(LocalDate date) {
+
+        List<Trip> res = repository.findByDate(date);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+
+    }
+
+    public ResponseEntity<?> getById(int id) {
+        Optional<Trip> res = repository.findById(id);
+        if (res.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+        }
+    }
+}
